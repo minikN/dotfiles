5e4b919ee9f0e8593c50463bca058e31860cff7b
Demis Balbach <demisbalbach@googlemail.com>
2020-03-23 11:50:36 +0100

    Simplify and streamline exwm keybindings

--- .doom.d/userconfig/exwm.el
+++ .doom.d/userconfig/exwm.el
@@ -1,35 +1,11 @@
 ;;; ~/.doom.d/userconfig/exwm.el -*- lexical-binding: t; -*-
 
-;; Functions to toggle normal mode in an x window
-(defun exwm/enter-normal-state ()
-  "Foward all commands to emacs by simulating normal mode"
-  (interactive)
-  (setq exwm-input-line-mode-passthrough t)
-  (evil-normal-state))
-
-(defun exwm/escape ()
-  "Switch to normal state, and cancel possible fullscreen layout.  Also close minibuffer."
-  (interactive)
-  (exwm/enter-normal-state)
-  (exwm-layout-unset-fullscreen)
-  (when (active-minibuffer-window)
-    (minibuffer-keyboard-quit)))
-
-(defun exwm/enter-insert-state ()
-  "Focus the x window again by simulating insert mode"
-  (interactive)
-  (setq exwm-input-line-mode-passthrough nil)
-  (evil-insert-state))
-
 ;; Emacs server is not required to run EXWM but it has some interesting uses
 ;; (see next section).
 (server-start)
 
 ;; Load EXWM.
 (require 'exwm)
-
-;; Fix problems with Ido (if you use it).
-(require 'exwm-config)
 
 ;; Set the initial number of workspaces (they can also be created later).
 (setq exwm-workspace-number 6)
@@ -47,44 +23,51 @@
                       (string= "gimp" exwm-instance-name))
               (exwm-workspace-rename-buffer exwm-title))))
 
-;; Get out of x window
-(exwm-input-set-key (kbd "s-<escape>") 'exwm/escape)
-
-;; Set shortcuts to switch to a certain workspace.
-(exwm-input-set-key (kbd "s-1")
-                    (lambda () (interactive) (exwm-workspace-switch 0)))
-(exwm-input-set-key (kbd "s-2")
-                    (lambda () (interactive) (exwm-workspace-switch 1)))
-(exwm-input-set-key (kbd "s-3")
-                    (lambda () (interactive) (exwm-workspace-switch 2)))
-(exwm-input-set-key (kbd "s-4")
-                    (lambda () (interactive) (exwm-workspace-switch 3)))
-(exwm-input-set-key (kbd "s-5")
-                    (lambda () (interactive) (exwm-workspace-switch 4)))
-(exwm-input-set-key (kbd "s-6")
-                    (lambda () (interactive) (exwm-workspace-switch 5)))
-(exwm-input-set-key (kbd "s-7")
-                    (lambda () (interactive) (exwm-workspace-switch 6)))
-(exwm-input-set-key (kbd "s-8")
-                    (lambda () (interactive) (exwm-workspace-switch 7)))
-(exwm-input-set-key (kbd "s-9")
-                    (lambda () (interactive) (exwm-workspace-switch 8)))
-(exwm-input-set-key (kbd "s-0")
-                    (lambda () (interactive) (exwm-workspace-switch 9)))
-
-;; Enter insert mode to work in x window again
-(evil-define-key 'normal exwm-mode-map (kbd "i") 'exwm/enter-insert-state)
+;; Disable the default key map
+(define-key exwm-mode-map (kbd "C-c") nil)
 
 ;; Global key bindings
 (setq exwm-input-global-keys
       `(
-        ;; Bind "s-r" to exit char-mode and fullscreen mode.
-        ([?\s-r] . exwm-reset)
-        ([?\s-w] . exwm-workspace-switch)
+        ;; Bind "s-0 -> s-9" to workspaces.
+        ([?\s-1] . (lambda () (interactive) (exwm-workspace-switch 0)))
+        ([?\s-2] . (lambda () (interactive) (exwm-workspace-switch 1)))
+        ([?\s-3] . (lambda () (interactive) (exwm-workspace-switch 2)))
+        ([?\s-4] . (lambda () (interactive) (exwm-workspace-switch 3)))
+        ([?\s-5] . (lambda () (interactive) (exwm-workspace-switch 4)))
+        ([?\s-6] . (lambda () (interactive) (exwm-workspace-switch 5)))
+        ([?\s-7] . (lambda () (interactive) (exwm-workspace-switch 6)))
+        ([?\s-8] . (lambda () (interactive) (exwm-workspace-switch 7)))
+        ([?\s-9] . (lambda () (interactive) (exwm-workspace-switch 8)))
+        ([?\s-0] . (lambda () (interactive) (exwm-workspace-switch 9)))
+
+        ;; Launch an application
         ([?\s-&] . (lambda (command)
                      (interactive (list (read-shell-command "$ ")))
                      (start-process-shell-command command nil command)))
+
+        ;; Enter line mode and redirect input to emacs
+        ([?\s-n] . (lambda () (interactive)
+                     (exwm-reset)
+                     (setq exwm-input-line-mode-passthrough t)))
+
+        ;; Only enter line mode
+        ([?\s-N] . (lambda () (interactive)
+                     (exwm-reset)
+                     (setq exwm-input-line-mode-passthrough nil)))
+
+        ;; enter char-mode again
+        ([?\s-i] . exwm-input-release-keyboard)
+        ([?\s-f] . exwm-layout-toggle-fullscreen)
+        ([?\s-t] . exwm-floating-toggle-floating)
+        ([?\s-m] . exwm-layout-toggle-mode-line)
+        ([?\s-w] . exwm-workspace-move-window)
         ))
+
+;; Set s-c and s-v to C-s and C-v in X application
+(setq exwm-input-simulation-keys
+      '(([?\s-c] . [C-c])
+        ([\?s-v] . [C-v])))
 
 ;; set char-mode to default
 (setq exwm-manage-configurations '((t char-mode t)))
