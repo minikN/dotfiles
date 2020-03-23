c34b3056720776218861c59f81e90cc469351f59
Demis Balbach <demisbalbach@googlemail.com>
2020-03-23 18:38:05 +0100

    Add function to display pinentry properly

--- .doom.d/userconfig/pinentry.el
+++ .doom.d/userconfig/pinentry.el
@@ -5,5 +5,18 @@
 (pinentry-start)
 
 ;; Update the TTY for gpg-agent
-(setenv "GPG_TTY" "/dev/pts/0")
-(shell-command "gpg-connect-agent updatestartuptty /bye >/dev/null")
+;(setenv "GPG_TTY" "/dev/pts/0")
+;(shell-command "gpgconf --launch-agent")
+;(shell-command "gpg-connect-agent updatestartuptty /bye >/dev/null")
+(defun wg/kludge-gpg-agent ()
+  (interactive)
+  (if
+      (display-graphic-p)
+      (setenv "DISPLAY"
+              (terminal-name))
+    (setenv "GPG_TTY"
+            (terminal-name))
+    (setenv "DISPLAY")))
+
+(advice-add 'magit-push :after #'wg/kludge-gpg-agent)
+;;(add-hook 'window-configuration-change-hook 'wg/kludge-gpg-agent)
