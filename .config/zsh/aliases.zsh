f02c6aa0df208cd4775969edeb3ac4a7fadb83ed
demis <demis@voidwalker.fritz.box>
2020-04-02 17:43:42 +0200

    Revert "update"
    This reverts commit c3c28bbe5d466377f7cc0d7eec399fdbe0ca1d93.

--- .config/zsh/aliases.zsh
+++
@@ -1,5 +0,0 @@
-# Aliases
-alias lss='ls --color=auto --group-directories-first -lAhgG'
-alias gpg2='gpg2 --homedir "$XDG_DATA_HOME"/gnupg'
-#alias xmas='startx "$XDG_CONFIG_HOME/X11/xinitrc" -- "$XDG_CONFIG_HOME/X11/xserverrc" vt1'
-alias xmas='startx "$XDG_CONFIG_HOME/X11/xinitrc" -- vt1'
--- .doom.d/userconfig/term.el
+++
@@ -1,3 +0,0 @@
-;;; ~/git/dotfiles/.doom.d/userconfig/term.el -*- lexical-binding: t; -*-
-
-(setq multi-term-program "/bin/zsh")
